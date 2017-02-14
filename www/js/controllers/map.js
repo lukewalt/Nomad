c9854348ef7ba97c8de5a31c8ecadd1166860e1c
Luke Woodward <luke.wdwrd@gmail.com>
2017-02-14 15:34:41 -0600

    implemented auto complete and search bar

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -1,130 +1,47 @@
 contrl.controller('GoogleMapCtrl', function($scope, $state, $cordovaGeolocation) {
-  console.log("map");
-  var options = {timeout: 10000, enableHighAccuracy: true};
+  //attaches autho complete to imnput field
+  const ac = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+  google.maps.event.addListener(ac, 'place_changed', function() {
+    let place = ac.getPlace();
+    console.log(place.formatted_address, place.url, place.geometry.location);
+  })
+  //sets high accuracy
+  let options = {timeout: 10000, enableHighAccuracy: true};
 
+  //DEVICE LOCATION: gets ^ options then it fires a function with a position of device
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
 
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-    var mapOptions = {
+    //sets device location to a google lat-lng and saves it as a variable
+    let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // establishes current options for current view
+    let mapOptions = {
       center: latLng,
       zoom: 11,
-      styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-
-
+    //NEW MAP:  sets options and display target for a new map to scope
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    //Wait until the map is loaded
+    //DROPS MARKER: drops marker on current location
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-      var marker = new google.maps.Marker({
-        map: $scope.map,
-        animation: google.maps.Animation.DROP,
-        position: latLng
-      });
-
-    //Wait until the map is loaded
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
       var marker = new google.maps.Marker({
           map: $scope.map,
           animation: google.maps.Animation.DROP,
           position: latLng
       });
-
       var infoWindow = new google.maps.InfoWindow({
           content: "Here I am!"
       });
-
       google.maps.event.addListener(marker, 'click', function () {
           infoWindow.open($scope.map, marker);
       });
-
     });
 
-    });
-  }, function(error){
+
+  },
+  //runs if cordova geolocation doesnt get current location
+  function(error){
     console.log("Could not get location");
   });
 
